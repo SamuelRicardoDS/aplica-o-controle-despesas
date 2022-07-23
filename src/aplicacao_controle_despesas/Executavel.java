8ef2a20391cf86d8b03e4e2312e60b7d21e550fb
SamuelRicardoDS <samuelricardods@gmail.com>
2022-07-23 10:31:46 -0300

    test getters and setters

--- src/aplicacao_controle_despesas/Executavel.java
+++ src/aplicacao_controle_despesas/Executavel.java
@@ -1,5 +1,15 @@
 package aplicacao_controle_despesas;
 
 public class Executavel {
+	public static void main(String[] args) {
+		
+		User input_user = new  User();
+		
+		input_user.setUsername(null);
+		input_user.setPassword(null);
+		
+		
+		
+	}
 
 }
