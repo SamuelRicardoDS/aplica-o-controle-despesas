45ade8b549a32fdcde15e25c4a489dc456e2166c
SamuelRicardoDS <samuelricardods@gmail.com>
2022-08-12 13:27:14 -0300

    images

--- src/view/ViewMain.java
+++ src/view/ViewMain.java
@@ -10,41 +10,41 @@ import javax.swing.JButton;
 
 public class ViewMain {
 
-	private JFrame frame;
-	private JLabel label;
-	private JButton btnBack;
-	
-	public static void main(String[] args) {
-		ViewMain window = new ViewMain();
-		window.frame.setVisible(true);
-	}
+    private JFrame frame;
+    private JLabel label;
+    private JButton btnBack;
+    
+    public static void main(String[] args) {
+        ViewMain window = new ViewMain();
+        window.frame.setVisible(true);
+    }
 
-	public ViewMain() {
-		
-		frame = new JFrame();
-		frame.setBounds(150, 150, 1280, 720);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
-		frame.setLocationRelativeTo(null);
-		
-				btnBack = new JButton("Voltar");
-				btnBack.setBounds(12, 646, 150, 25);
-				frame.getContentPane().add(btnBack);
-				
-						btnBack.addActionListener(arg0 -> {
-							ViewLogin window = new ViewLogin();
-							window.getLoginFrame().setVisible(true);
-							frame.dispose();
-						});
-		
-		label = new JLabel("");
-		label.setIcon(new ImageIcon(ViewMain.class.getResource("/images/fc.jpg")));
-		label.setBounds(0, -24, 1280, 707);
-		frame.getContentPane().add(label);
-		
+    public ViewMain() {
+        
+        frame = new JFrame();
+        frame.setBounds(150, 150, 1280, 720);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().setLayout(null);
+        frame.setLocationRelativeTo(null);
+        
+                btnBack = new JButton("Voltar");
+                btnBack.setBounds(12, 646, 150, 25);
+                frame.getContentPane().add(btnBack);
+                
+                        btnBack.addActionListener(arg0 -> {
+                            ViewLogin window = new ViewLogin();
+                            window.getLoginFrame().setVisible(true);
+                            frame.dispose();
+                        });
+        
+        label = new JLabel("");
+        label.setIcon(new ImageIcon(ViewMain.class.getResource("/images/fc.jpeg")));
+        label.setBounds(0, -24, 1280, 707);
+        frame.getContentPane().add(label);
+        
 
-	}
-	public JFrame getMainFrame() {
-		return frame;
-	}
+    }
+    public JFrame getMainFrame() {
+        return frame;
+    }
 }
