098ae422c4450647ae20e8ae437736e803a50514
GustavoHenrique23 <gustavohenriqueprivado@gmail.com>
2022-08-23 00:15:37 -0300

    duzao

--- src/view/ViewCreateGroup.java
+++ src/view/ViewCreateGroup.java
@@ -6,21 +6,31 @@ import java.awt.BorderLayout;
 import javax.swing.JTextField;
 import javax.swing.JTextArea;
 import java.awt.Label;
-import com.jgoodies.forms.factories.DefaultComponentFactory;
+import javax.swing.JButton;
+import javax.swing.ImageIcon;
+
+
+
 
 public class ViewCreateGroup {
+	
+	public static void main(String[] args) {
+	    ViewCreateGroup window = new ViewCreateGroup();
+	    window.frame.setVisible(true);
+	}
 
 	private JFrame frame;
-
+	private JLabel background;
 	public ViewCreateGroup() {
 		
 		frame = new JFrame();
-		frame.setBounds(100, 100, 852, 480);
+		frame.setBounds(100, 100, 800, 480);
 		frame.getContentPane().setLayout(null);
 		
-		JLabel label = new JLabel("");
-		label.setBounds(153, 100, 70, 15);
-		frame.getContentPane().add(label);
+		background = new JLabel("");
+		background.setBounds(0, -28, 925, 542);
+		frame.getContentPane().add(background);
+		background.setIcon(new ImageIcon(ViewCreateGroup.class.getResource("/images/duzao.jpg")));
 		frame.setLocationRelativeTo(null);
 		
 		
