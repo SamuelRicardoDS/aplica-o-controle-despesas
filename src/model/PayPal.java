0ee99c7a69255a7325f678197b07ce600af5d9c5
SamuelRicardoDS <samuelricardods@gmail.com>
2022-09-02 13:06:17 -0300

    override finished

--- src/model/PayPal.java
+++ src/model/PayPal.java
@@ -1,14 +1,26 @@
 package model;
+
 import java.util.Date;
 
 public class PayPal extends PaymentMethod {
   private String cardFlag;
   private String type;
-  
+
   public PayPal(Date payDate, String bank, String payer, String cardFlag, String type, Double amount) {
     super(payDate, bank, payer, amount);
     this.cardFlag = cardFlag;
     this.type = type;
+  }
+
+  @Override
+  public Double calcSingleValue(Group group) {
+    amount = 0.0;
+    int integers = group.getMembers().size();
+    for (int i = 0; i < integers; i++) {
+      amount += group.getExpenses().get(i).getValue();
+    }
+    Double singleValue = amount / integers;
+    return singleValue * 1.035;
   }
 
   public String getCardFlag() {
@@ -26,6 +38,5 @@ public class PayPal extends PaymentMethod {
   public void setType(String type) {
     this.type = type;
   }
-  
-}
 
+}
