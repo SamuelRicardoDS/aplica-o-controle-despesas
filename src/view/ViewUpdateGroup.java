642ec22f020f505d09f3de9571cf40257a376e1f
GustavoHenrique23 <gustavohenriqueprivado@gmail.com>
2022-09-09 15:30:37 -0300

    Update

--- src/view/ViewUpdateGroup.java
+++ src/view/ViewUpdateGroup.java
@@ -3,14 +3,10 @@ package view;
 import controller.*;
 import java.awt.Color;
 import java.awt.event.*;
-import java.util.ArrayList;
-import javax.swing.DefaultComboBoxModel;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
-import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JList;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
@@ -18,7 +14,6 @@ import javax.swing.border.LineBorder;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import model.Group;
-import model.User;
 
 /**
  * Classe responsável pela tela de atualização de grupos.
@@ -40,10 +35,34 @@ public class ViewUpdateGroup {
     private JButton btnRemoveMembers;
     private JScrollPane tabela;
     private JButton btnBack;
-    private JTable table_1;
-    private JScrollPane scrollPane;
-    private JTable table_2;
+
+    private JTable membros;
     private JScrollPane scrollPane_1;
+
+    private JTable friends;
+    private JScrollPane scrollPane_2;
+
+    private void UpdateMembersTable() {
+        int selectedRow = table.getSelectedRow();
+        Group selectedGroup = cu.readOne(cu.getUsername()).getGroups().get(selectedRow == -1 ? 0 : selectedRow);
+
+        String[] columns1 = { "Membros do Grupo" };
+
+        Object[][] data1 = new String[cu
+            .readOne(cu.getUsername())
+            .getGroups()
+            .get(selectedRow)
+            .getMembers()
+            .size()][columns1.length];
+
+        for (int i = 0; i < selectedGroup.getMembers().size(); i++) {
+            String[] members = { selectedGroup.getMembers().get(i).getUsername() };
+            data1[i] = members;
+        }
+        membros = new JTable(data1, columns1);
+        scrollPane_1.setViewportView(membros);
+        membros.repaint();
+    }
 
     public ViewUpdateGroup() {
         cu = new ControlUser();
@@ -79,65 +98,70 @@ public class ViewUpdateGroup {
                 new ListSelectionListener() {
 
                     public void valueChanged(ListSelectionEvent event) {
-                        int selectedRow = table.getSelectedRow();
-                        Group selectedGroup = cu
-                            .readOne(cu.getUsername())
-                            .getGroups()
-                            .get(selectedRow == -1 ? 0 : selectedRow);
-                        String[] columns1 = { "Membros do Grupo" };
-
-                        Object[][] data1 = new String[cu
-                            .readOne(cu.getUsername())
-                            .getGroups()
-                            .get(selectedRow)
-                            .getMembers()
-                            .size()][columns1.length];
-
-                        for (int i = 0; i < selectedGroup.getMembers().size(); i++) {
-                            String[] members = { selectedGroup.getMembers().get(i).getUsername() };
-                            data1[i] = members;
-                        }
-                        table_2 = new JTable(data1, columns1);
-                        scrollPane_1.setViewportView(table_2);
-                        table_2.repaint();
+                        UpdateMembersTable();
                     }
                 }
             );
 
         textField = new JTextField();
-        textField.setBounds(330, 50, 170, 26);
+        textField.setBounds(330, 88, 170, 26);
         frame.getContentPane().add(textField);
         textField.setColumns(10);
 
         btnRemoveMembers = new JButton("Remover Membros");
-        btnRemoveMembers.setBounds(558, 190, 198, 26);
+        btnRemoveMembers.setBounds(558, 364, 198, 26);
         frame.getContentPane().add(btnRemoveMembers);
 
         btnRemoveMembers.addActionListener(
             new ActionListener() {
 
-                public void actionPerformed(ActionEvent e) {}
+                public void actionPerformed(ActionEvent e) {
+                    int selectedIndex = membros.getSelectedRow();
+                    if (selectedIndex < 0) {
+                        return;
+                    }
+                    cu
+                        .readOne(cu.getUsername())
+                        .getGroups()
+                        .get(table.getSelectedRow())
+                        .getMembers()
+                        .remove(membros.getSelectedRow());
+                    UpdateMembersTable();
+                }
             }
         );
 
         addNewMembers = new JButton("Adicionar membros");
-        addNewMembers.setBounds(558, 382, 198, 26);
+        addNewMembers.setBounds(558, 401, 198, 26);
         frame.getContentPane().add(addNewMembers);
 
         addNewMembers.addActionListener(
             new ActionListener() {
 
-                public void actionPerformed(ActionEvent e) {}
+                public void actionPerformed(ActionEvent e) {
+                    int friendIndex = friends.getSelectedRow();
+                    if (friendIndex < 0) {
+                        return;
+                    }
+
+                    cu
+                        .readOne(cu.getUsername())
+                        .getGroups()
+                        .get(table.getSelectedRow())
+                        .getMembers()
+                        .add(cu.readOne(cu.getUsername()).getFriends().get(friendIndex));
+                    UpdateMembersTable();
+                }
             }
         );
 
         ChangeName = new JLabel("Alterar Nome");
         ChangeName.setForeground(Color.WHITE);
-        ChangeName.setBounds(334, 35, 166, 15);
+        ChangeName.setBounds(330, 61, 166, 15);
         frame.getContentPane().add(ChangeName);
 
         buttonChangeName = new JButton("Alterar Nome");
-        buttonChangeName.setBounds(352, 84, 136, 25);
+        buttonChangeName.setBounds(351, 126, 136, 25);
         frame.getContentPane().add(buttonChangeName);
 
         buttonChangeName.addActionListener(
@@ -172,14 +196,29 @@ public class ViewUpdateGroup {
         }
 
         scrollPane_1 = new JScrollPane();
-        scrollPane_1.setBounds(558, 35, 198, 141);
+        scrollPane_1.setBounds(558, 35, 198, 317);
         frame.getContentPane().add(scrollPane_1);
 
         String[] columns1 = { "Membros do Grupo" };
         Object[][] data1 = new String[0][columns1.length];
 
-        table_2 = new JTable(data1, columns1);
-        scrollPane_1.setViewportView(table_2);
+        membros = new JTable(data1, columns1);
+        scrollPane_1.setViewportView(membros);
+
+        scrollPane_2 = new JScrollPane();
+        scrollPane_2.setBounds(334, 177, 166, 250);
+        frame.getContentPane().add(scrollPane_2);
+
+        String[] column_3 = { "Lista de Amigos" };
+        Object[][] data_3 = new String[cu.readOne(cu.getUsername()).getFriends().size()][column_3.length];
+
+        for (int i = 0; i < cu.readOne(cu.getUsername()).getFriends().size(); i++) {
+            String[] friends = { cu.readOne(cu.getUsername()).getFriends().get(i).getUsername() };
+            data_3[i] = friends;
+        }
+
+        friends = new JTable(data_3, column_3);
+        scrollPane_2.setViewportView(friends);
 
         background = new JLabel("");
         background.setBounds(0, 0, 800, 480);
