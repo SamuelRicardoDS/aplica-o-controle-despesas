3d7ded54ca123e1975684d0cec4dc1d946f552ce
SamuelRicardoDS <samuelricardods@gmail.com>
2022-09-11 16:52:34 -0300

    rlider and hmuo working together

--- src/controller/ControlUser.java
+++ src/controller/ControlUser.java
@@ -16,15 +16,16 @@ public class ControlUser {
         this.database = Database.getInstance();
     }
 
-    public void create(String username, String password, ArrayList<Group> groups, ArrayList<User> friends, PaymentMethod paymentMethod) {
-        if(username == null || password == null) {
+    public void create(String username, String password, ArrayList<Group> groups, ArrayList<User> friends,
+            PaymentMethod paymentMethod) {
+        if (username == null || password == null) {
             System.out.println("Preencha todos os campos");
         } else {
             User user = new User(username, password, groups, friends, paymentMethod);
             database.addUser(user);
             System.out.println("Usuário cadastrado com sucesso");
         }
-       System.out.println(database.getUsers());
+        System.out.println(database.getUsers());
     }
 
     public User readOne(String username) {
@@ -43,7 +44,6 @@ public class ControlUser {
         database.getUsers().remove(user);
     }
 
-
     public Boolean controlLogin(String username, String password) {
         Boolean isValidUser = false;
 
@@ -55,50 +55,63 @@ public class ControlUser {
         }
         return isValidUser;
     }
-    
+
     public Boolean verifyFriend(String username) {
-    	Boolean isValidFriend = false;
-    	for(User user : this.database.getUsers()) {
-    		if(user.getUsername().equals(username)) {
-    			isValidFriend = true;
-    		}
-    	}
-    	return isValidFriend;
+        Boolean isValidFriend = false;
+        for (User user : this.database.getUsers()) {
+            if (user.getUsername().equals(username)) {
+                isValidFriend = true;
+            }
+        }
+        return isValidFriend;
     }
+
     public String getUsername() {
         return username;
     }
-    
+
     public void addFriend(String friend, User user) {
         user.getFriends().add(this.readOne(friend));
         readOne(friend).getFriends().add(user);
     }
-    //public String definePassword
+    // public String definePassword
 
-  /*   public Double calculateTotalValue() {
-        Double totalValue = 0.0;
-        for (User user : this.database.getUsers()) {
-            for(int i=0; i<user.getGroups().size(); i++) {
-                totalValue += user.getGroups().get(i).calcSingleValue();
-            }
-        }
-        return totalValue;
-    } */
+    /*
+     * public Double calculateTotalValue() {
+     * Double totalValue = 0.0;
+     * for (User user : this.database.getUsers()) {
+     * for(int i=0; i<user.getGroups().size(); i++) {
+     * totalValue += user.getGroups().get(i).calcSingleValue();
+     * }
+     * }
+     * return totalValue;
+     * }
+     */
 
-    
-
-	public ArrayList<User> readAll() {
+    public ArrayList<User> readAll() {
         return this.database.getUsers();
+    }
+
+    public Boolean RLider(Group group, User user) {
+        Boolean isLider = false;
+        if (group.getLider().getUsername().equals(user.getUsername())) {
+            isLider = true;
+        }
+        return isLider;
     }
 
     public Double howMuchUOwe(User user) {
         Double owe = 0.0;
-        for(Group group : user.getGroups()) {
-            Double amount = group.totalValue(group.getExpenses());
-            int integers = group.getMembers().size();
-            owe += amount / (1 + integers);
+        for (Group group : user.getGroups()) {
+            if (RLider(group, user) == true) {
+                owe = 0.0;
+            } else {
+                Double amount = group.totalValue(group.getExpenses());
+                int integers = group.getMembers().size();
+                owe += amount / (1 + integers);
+            }
         }
         return owe;
     }
-    
+
 }
